2e348719a3ffda9878c9a67b595003b2998890cc
faradibaa <faradiba.annisa2@gmail.com>
2023-07-20 14:39:40 +0700

    Edit

--- contracts/Converter.sol
+++ contracts/Converter.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.13;
 
 contract Converter {
-    function weiToEth(uint amount) public pure returns(uint) {
+    function weiToEth(uint amount) internal pure returns(uint) {
         return amount / 1000000000000000000; // 1 ETH = 1,000,000,000,000,000,000 WEI
     }
 }
